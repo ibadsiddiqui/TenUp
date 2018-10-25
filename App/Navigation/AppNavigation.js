20dfd209009fb21930ef348b822a5f55a511839b
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-10-25 17:05:21 +0500

    debugging

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -6,20 +6,20 @@ import { StackNavigator } from 'react-navigation'
 // import RegistrationScreen from './../Containers/Screens/RegisterationScreen'
 // import ProfileScreen from './../Containers/Screens/ProfileScreen'
 // import ChangePasswordScreen from './../Containers/Screens/ChangePasswordScreen';
-// import NoWifiScreen from './../Containers/Screens/NoWifiScreen';
+import NoWifiScreen from './../Containers/Screens/NoWifiScreen';
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  SetPINScreen: { screen: SetPINScreen },
-  ConfirmPINScreen: {
-    screen: ConfirmPINScreen,
-    headerLeft: null
-  },
   NoWifiScreen: { screen: NoWifiScreen },
-  LoginScreen: { screen: LoginScreen },
-  RegistrationScreen: { screen: RegistrationScreen },
-  ProfileScreen: { screen: ProfileScreen },
-  ChangePassswordScreen: { screen: ChangePasswordScreen }
+  // SetPINScreen: { screen: SetPINScreen },
+  // ConfirmPINScreen: {
+  //   screen: ConfirmPINScreen,
+  //   headerLeft: null
+  // },
+  // LoginScreen: { screen: LoginScreen },
+  // RegistrationScreen: { screen: RegistrationScreen },
+  // ProfileScreen: { screen: ProfileScreen },
+  // ChangePassswordScreen: { screen: ChangePasswordScreen }
 }, {
     // Default config for all screens
     headerMode: 'none',
