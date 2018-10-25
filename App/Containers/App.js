1d106cfbccb95f7c2c7e85790bad8217c61b1a87
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-10-25 16:58:55 +0500

    Refactored to Stack Navigation

---
+++ App/Containers/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import PrimaryNav from './../Navigation/AppNavigation'
+export default class App extends Component {
+  render() {
+    return (
+      <PrimaryNav/>
+    )
+  }
+}
