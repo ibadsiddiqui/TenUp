2b42232c6ffa446dc82f8c716209a0f1b484b3a5
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-11-05 17:59:49 +0500

    HomeScreen added to dashboard

--- App/Containers/Screens/DashboardScreen.js
+++ App/Containers/Screens/DashboardScreen.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import {
     View,
+    Image,
     StatusBar,
-    Image
 } from 'react-native'
 import styles from './../Styles/DashboardScreenStyles'
+
+import HomeScreen from './../../Component/HomeScreen/index'
 
 export default class DashboardScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
                 <StatusBar translucent={true} backgroundColor="transparent" />
+
+                <HomeScreen/>
                 <View style={styles.lowerNavBar}>
                     <View style={styles.iconsContainer}>
                         <Image source={require('./../../Assets/Bottam_tab-assets/sent_select.png')} style={styles.lowerNavBarIcons} />
@@ -29,7 +33,6 @@ export default class DashboardScreen extends Component {
                     </View>
                     <View style={styles.iconsContainer}>
                         <Image source={require('./../../Assets/Bottam_tab-assets/setting_select.png')} style={styles.lowerNavBarIcons} />
-
 
                     </View>
                 </View>
