9b9484cf2f269f49e9647a88c9c2884f244fed38
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-11-05 18:09:23 +0500

    style for flatlist updated

--- App/Component/HomeScreen/components/FlatListItem/styles.js
+++ App/Component/HomeScreen/components/FlatListItem/styles.js
@@ -6,30 +6,7 @@ const styles = StyleSheet.create({
     rowView: {
         flexDirection: 'row',
     },
-    TupInfoMargin: {
-        marginTop: height * 0.31
-    },
-    tupInfoContainer: {
-        marginLeft: width * 0.65,
-        position: 'absolute'
-    },
-    tupInfoTUPstyle: {
-        fontWeight: 'bold',
-        marginTop: height * 0.003,
-        fontSize: width * 0.05,
 
-    },
-    tupInfoTupCurrency: {
-        color: '#499FDB',
-        fontWeight: 'bold',
-        fontSize: width * 0.04,
-        marginTop: height * 0.01,
-    },
-    tupInfoUSDCurrency: {
-        fontWeight: 'bold',
-        fontSize: width * 0.06,
-        marginTop: height * 0.01,
-    },
     tableMargin: {
         marginLeft: width * 0.1,
     },
@@ -55,22 +32,11 @@ const styles = StyleSheet.create({
         marginBottom: height * 0.02,
         fontSize: height * 0.02,
     },
-    tableBorder: {
-        borderTopColor: '#499FDB',
-        borderTopWidth: 1,
-    },
     tableShadowBox: {
         borderWidth: 1,
-        borderRadius: 2,
         borderColor: 'rgb(208,208,208)',
         borderBottomWidth: 0,
     },
-    transactionScroll: {
-        marginTop: height * 0.56,
-        position: 'absolute',
-        width: width,
-        height: height * 0.35,
-    }
 
 });
 
