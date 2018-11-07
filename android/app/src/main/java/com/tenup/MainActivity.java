af027642c1aeae1c56fba6a268e589ad699d609b
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-11-07 10:02:45 +0500

    Facebook Login added

--- android/app/src/main/java/com/tenup/MainActivity.java
+++ android/app/src/main/java/com/tenup/MainActivity.java
@@ -3,6 +3,7 @@ package com.tenup;
 import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen; // here
+import android.content.Intent;
 public class MainActivity extends ReactActivity {
 
     /**
@@ -14,6 +15,12 @@ public class MainActivity extends ReactActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this); // here
         super.onCreate(savedInstanceState);
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
--- android/app/src/main/java/com/tenup/MainApplication.java
+++ android/app/src/main/java/com/tenup/MainApplication.java
@@ -2,7 +2,12 @@ package com.tenup;
 
 import android.app.Application;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -14,6 +19,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -23,8 +34,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SplashScreenReactPackage()
+        new MainReactPackage(), 
+        new FBSDKPackage(mCallbackManager), 
+        new SplashScreenReactPackage()
       );
     }
 
@@ -43,5 +55,6 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+    AppEventsLogger.activateApp(this);
   }
 }
